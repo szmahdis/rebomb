fa8b832b9aa95a068c4efb6ee726a08c68362c74
Mahdis <szmahdis@gmail.com>
2024-11-17 00:53:30 +0100

    simple gui, update gitignore
    add simple gui, update git ignore

---
+++ Rebomb/Assets/Script/MainMenu.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        Debug.Log("Starting the game!");
+        SceneManager.LoadSceneAsync("SampleScene");
+    }
+
+    public void OpenOptionsMenu()
+    {
+        Debug.Log("Switching to options menu!");
+        SceneManager.LoadSceneAsync("OptionsMenu");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting the game!");
+        Application.Quit();
+    }
+}
